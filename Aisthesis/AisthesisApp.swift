c80d3ffbb55f0601ca9065fe80b032646cbd97e7
ver38 <116646462+ver38@users.noreply.github.com>
2024-03-13 19:23:37 +0100

    Initial commit

---
+++ Aisthesis/AisthesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AisthesisApp.swift
+//  Aisthesis
+//
+//  Created by Veronica Natale on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AisthesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
